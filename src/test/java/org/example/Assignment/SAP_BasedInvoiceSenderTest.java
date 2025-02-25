d9c77705b58378e87596b977c40244d698a89d91
rhur4 <ryhurtado2004@gmail.com>
2025-02-24 22:46:59 -0600

    Rylan Hurtado - Improved error handling and added exception testing
    Signed-off-by: rhur4 <ryhurtado2004@gmail.com>

--- src/test/java/org/example/Assignment/SAP_BasedInvoiceSenderTest.java
+++ src/test/java/org/example/Assignment/SAP_BasedInvoiceSenderTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 public class SAP_BasedInvoiceSenderTest {
@@ -13,11 +14,14 @@ public class SAP_BasedInvoiceSenderTest {
     /**
      *  Mocks FilterInvoice and SAP objects to create a controlled SAP_BasedInvoiceSender object. Stubs the return value
      *  of lowValueInvoices() to a preselected invoices list when being calling in sendLowValuedInvoices(). Verifies
-     *  that the sender sent each invoice to the SAP and that the correct total number of invoices were sent.
+     *  that the sender sent each invoice to the SAP and that the correct total number of invoices were sent. (Modified
+     *  to propagate FailToSendSAPInvoiceException from refactored SAP_BasedInvoiceSender class.)
+     *
+     *  @throws FailToSendSAPInvoiceException
      */
     @Test
     @Tag("Mocking")
-    void testWhenLowInvoicesSent() {
+    void testWhenLowInvoicesSent() throws FailToSendSAPInvoiceException {
         FilterInvoice fi = mock(FilterInvoice.class);
         SAP sap = mock(SAP.class);
 
@@ -42,11 +46,14 @@ public class SAP_BasedInvoiceSenderTest {
 
     /**
      *  Similar configurations to the above test, only now verifying that the sender sent no invoices to SAP if there
-     *  are no low-valued invoices.
+     *  are no low-valued invoices. (Modified to propagate FailToSendSAPInvoiceException from refactored
+     *  SAP_BasedInvoiceSender class.)
+     *
+     *  @throws FailToSendSAPInvoiceException
      */
     @Test
     @Tag("Mocking")
-    void testWhenNoInvoices() {
+    void testWhenNoInvoices() throws FailToSendSAPInvoiceException {
         FilterInvoice fi = mock(FilterInvoice.class);
         SAP sap = mock(SAP.class);
 
@@ -59,5 +66,36 @@ public class SAP_BasedInvoiceSenderTest {
         sender.sendLowValuedInvoices();
 
         verify(sap, never()).send(any(Invoice.class));
+    }
+
+    /**
+     *  Similar configurations to the above tests, now handling a FailToSendSAPInvoiceException to be thrown when
+     *  stubbing the first element of the invoices list. Verifies that, although send() was attempted on all three
+     *  elements, the list of failed sends correctly only contains the first element.
+     *
+     *  @throws FailToSendSAPInvoiceException
+     */
+    @Test
+    @Tag("Mocking")
+    void testThrowExceptionWhenBadInvoice() throws FailToSendSAPInvoiceException {
+        FilterInvoice fi = mock(FilterInvoice.class);
+        SAP sap = mock(SAP.class);
+
+        List<Invoice> invoices = Arrays.asList(
+                new Invoice("customer1", 100),
+                new Invoice("customer2", 50),
+                new Invoice("customer3", 75)
+        );
+
+        when(fi.lowValueInvoices()).thenReturn(invoices);
+        doThrow(new FailToSendSAPInvoiceException("fail")).when(sap).send(invoices.getFirst());
+
+        SAP_BasedInvoiceSender sender = new SAP_BasedInvoiceSender(fi, sap);
+
+        List<Invoice> failedSends = sender.sendLowValuedInvoices();
+
+        verify(sap, times(3)).send(any(Invoice.class));
+        assertThat(failedSends.size()).isEqualTo(1);
+        assertThat(failedSends).contains(invoices.getFirst());
     }
 }
